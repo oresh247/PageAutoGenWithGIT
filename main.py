97bb2097a33834f48713a89e3758b346a818648e
oresh247 <oresh247@gmail.com>
2024-12-26 12:50:24 +0300

    Добавлена поставка OKR_20250302_ATM

--- main.py
+++ main.py
@@ -616,7 +616,7 @@ def get_release_test_cases(release):
             return test_cases['content']
     return ''
 
-release = 'OKR_20250125_ATM' # Метка релиза
+release = 'OKR_20250302_ATM' # Метка релиза
 for_publication_flg = True # Если True - то публикуем, если False, только возврат списка задач
 replace_flg = True # Если True - то заменяем содержимое страницы
 update_story_flg = False  # Если True - обновляем спиисок задач в story (удаляем все и добавляем те, что в текущем релизе)
